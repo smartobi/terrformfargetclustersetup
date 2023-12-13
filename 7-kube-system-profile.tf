74ab016e93a9ca10820764e26b65bb69f27b501c
smartobi <agbawochinonso488@gmail.com>
2023-12-13 09:59:01 -0800

    added gitaction file

--- 7-kube-system-profile.tf
+++ 7-kube-system-profile.tf
@@ -89,3 +89,22 @@ resource "aws_eks_fargate_profile" "staging" {
     namespace = "staging"
   }
 }
+
+
+
+resource "aws_eks_fargate_profile" "argocd" {
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "argocd"
+  pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
+
+  # These subnets must have the following resource tag: 
+  # kubernetes.io/cluster/<CLUSTER_NAME>.
+  subnet_ids = [
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
+  ]
+
+  selector {
+    namespace = "argocd"
+  }
+}
--- 9-metrics-server.tf
+++ 9-metrics-server.tf
@@ -1,28 +1,28 @@
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
-      command     = "aws"
-    }
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
+#       command     = "aws"
+#     }
+#   }
+# }
 
 
-resource "helm_release" "metrics-server" {
-  name = "metrics-server"
+# resource "helm_release" "metrics-server" {
+#   name = "metrics-server"
 
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "metrics-server"
-  namespace  = "kube-system"
-  version    = "6.6.5"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "metrics-server"
+#   namespace  = "kube-system"
+#   version    = "6.6.5"
 
-  set {
-    name  = "metrics.enabled"
-    value = true
-  }
+#   set {
+#     name  = "metrics.enabled"
+#     value = true
+#   }
 
-  depends_on = [aws_eks_fargate_profile.kube-system]
-}
+#   depends_on = [aws_eks_fargate_profile.kube-system]
+# }
