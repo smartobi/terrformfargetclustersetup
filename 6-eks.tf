6bff0a7e8b23fdeaa43bad8712e5b0d707df57d0
smartobi <agbawochinonso488@gmail.com>
2023-12-13 09:42:10 -0800

    added gitaction file

--- 6-eks.tf
+++ 6-eks.tf
@@ -19,7 +19,7 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "amazon-eks-cluster-policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks-cluster.name
+  role       = aws_iam_role.eks-cluster.nam
 }
 
 
